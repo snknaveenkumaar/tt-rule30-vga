f8f0042ecaf857acbf8d2d7f3157156eea9ef2b4
snknaveenkumaar <snkwariors98@gmail.com>
2026-03-08 01:06:45 +0530

    Update and rename tb.v to tb.py

---
+++ test/tb.py
@@ -0,0 +1,24 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge
+
+@cocotb.test()
+async def test_project(dut):
+
+    clock = Clock(dut.clk, 40, units="ns")
+    cocotb.start_soon(clock.start())
+
+    dut.ena.value = 1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+
+    for _ in range(10):
+        await RisingEdge(dut.clk)
+
+    dut.rst_n.value = 1
+
+    for _ in range(100):
+        await RisingEdge(dut.clk)
+
+    assert True
